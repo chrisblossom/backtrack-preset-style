dd6dd25b98651bf029dc9da7650a91c4764a9ff1
Chris Blossom <cbloss@gmail.com>
2023-10-24 13:53:41 -0700

    prettier v3

--- .backtrackrc.js
+++ .backtrackrc.js
@@ -1,12 +1,34 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = {
 	presets: ['@backtrack/preset'],
 
-	// use local eslint config
 	config: {
+		// use local eslint config
 		eslint: () => ({
 			extends: '@chrisblossom/eslint-config/node',
 		}),
+		// use local prettier config
+		prettier: (config) => {
+			config.plugins = config.plugins.map((plugin) =>
+				plugin
+					.split(path.sep)
+					.find((name) => name.includes('prettier-plugin-')),
+			);
+
+			return config;
+		},
 	},
+
+	files: [
+		{ src: 'lib/files/prettier.cjs', dest: '.prettierrc.cjs' },
+		{
+			skip: ['.prettierrc.js'],
+			allowChanges: [
+				'.prettierignore',
+			],
+		},
+	],
 };
